e6058aaa8f2c63d364c9f9bacc5bb4fe88636b8c
Kawser Ahmed <32969466+kawserahmed47@users.noreply.github.com>
2019-10-17 01:00:26 +0600

    Add files via upload

---
+++ precache-manifest.96f89c636e71cfa250c3df0263ae4b13.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "057b76a328110d6beb776458cae790d5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bf691e122c56857ac88b",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "90664925ad0f6ebd37cc",
+    "url": "/static/css/main.543edb8d.chunk.css"
+  },
+  {
+    "revision": "bf691e122c56857ac88b",
+    "url": "/static/js/2.a9a25162.chunk.js"
+  },
+  {
+    "revision": "90664925ad0f6ebd37cc",
+    "url": "/static/js/main.f4f030ee.chunk.js"
+  },
+  {
+    "revision": "7a63c5ab0b09f70a3133",
+    "url": "/static/js/runtime-main.4dbf9e79.js"
+  }
+]);
